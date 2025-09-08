bfb5f53d4b1c4b0934f32543343ebdede4bdf0b5
OmarHassan <omar13102005@gmail.com>
2025-09-09 01:47:45 +0300

    feat: create CTA buttons in hero section

--- src/routes/index/~components/hero.tsx
+++ src/routes/index/~components/hero.tsx
@@ -1,6 +1,7 @@
 import { useSections } from "../~hooks/useSections";
 import Overlay from "./Overlay";
 import { useEffect, useRef } from "react";
+import { Button } from "@/components/ui/button";
 
 export default function Hero() {
   const { updateSection } = useSections()
@@ -16,11 +17,15 @@ export default function Hero() {
       <Overlay />
       {/* <span>Hey, I am</span> */}
       <div className="max-w-260 z-elevated">
-        <h1 className="text-7xl font-bold text-[#9AD1D4] leading-[1.1]">Hi, I'm Omar Hassan.</h1>
-        <p className="text-[#9AD1D4]/80 font-bold text-7xl leading-[1.1]">An Eager Frontend Developer.</p>
-        <p className="text-[#9AD1D4]/70 text-xl mt-8 text-balance">
+        <h1 className="text-7xl font-bold text-primary leading-[1.1]">Hi, I'm Omar Hassan.</h1>
+        <p className="text-primary/80 font-bold text-7xl leading-[1.1]">An Eager Frontend Developer.</p>
+        <p className="text-primary/70 text-xl mt-8 text-balance">
           I am a frontend engineer with some backend experience, passionate about creating high-notch applications. I focus on making apps performant, accessible, responsive, and highly user-friendly. I care deeply about my work and always put in the extra effort, constantly aspiring to improve and be a better version of myself.
         </p>
+        <div className="mt-8 flex gap-4">
+          <Button variant="outline" size="lg">Say Hi</Button>
+          <Button variant="outline" size="lg">Download CV</Button>
+        </div>
       </div>
     </div>
   )
